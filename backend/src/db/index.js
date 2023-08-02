c47df1db625314eb1191d13dc316ad6085bbf899
saed <saedabdi3@gmail.comm>
2023-08-02 14:24:00 -0400

    index

--- backend/src/db/index.js
+++ backend/src/db/index.js
@@ -1,15 +1,16 @@
-const { Pool } = require('pg');
+const pg = require("pg");
 
-const dbParams = {
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.DB_NAME
-};
+const client = new pg.Client({
+  host: process.env.PGHOST,
+  name: process.env.PGDATABASE,
+  user: process.env.PGUSER,
+  password: process.env.PGPASSWORD,
+  port: process.env.PGPORT,
+  ssl: process.env.DATABASE_URL ? { rejectUnauthorized: false } : false
+});
 
-const db = new Pool(dbParams);
+client
+  .connect()
+  .catch(e => console.log(`Error connecting to Postgres server:\n${e}`));
 
-db.connect();
-
-module.exports = db;
+module.exports = client;
--- backend/src/index.js
+++ backend/src/index.js
@@ -1,9 +1,8 @@
-require('dotenv').config();
 const PORT = process.env.PORT || 8001;
-const ENV = require("./environment");
+const ENV = require('./environment');
 
-const app = require("./application")(ENV);
-const server = require("http").Server(app);
+const app = require('./application')(ENV);
+const server = require('http').Server(app);
 
 server.listen(PORT, () => {
   console.log(`Listening on port ${PORT} in ${ENV} mode.`);
